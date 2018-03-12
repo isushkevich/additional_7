31da58664b48e71c7e9465c8c364c1a652726c79
isushkevich <ivan.sushik@gmail.com>
2018-03-13 01:59:45 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+module.exports = function solveSudoku( matrix ) {
+
+  const emptyCells = [];
+
+  for ( let i = 0; i < 9; ++i ) {
+    for ( let j = 0; j < 9; ++j ) {
+      if ( matrix[ i ][ j ] == 0 ) {
+        emptyCells.push( [ i, j ] );
+      }
+    }
+  }
+
+  let row, column, value, found, emptyCount = emptyCells.length;
+
+  for ( let i = 0; i < emptyCount; ++i ) {
+    row = emptyCells[ i ][ 0 ];
+    column = emptyCells[ i ][ 1 ];
+
+    value = matrix[ row ][ column ] + 1;
+
+    found = false;
+
+    while ( !found && value <= 9 ) {
+
+      if ( checkSudoku( matrix, row, column, value ) ) {
+        found = true;
+        matrix[ row ][ column ] = value;
+      } else {
+        value++;
+      }
+    }
+
+    if ( !found ) {
+      matrix[ row ][ column ] = 0;
+      i--;
+    }
+  }
+
+  return matrix;
+
+}
+
+
+function checkSudoku( matrix, row, column, candidate ) {
+
+  for ( let i = 0; i < 9; ++i ) {
+    if ( matrix[ row ][ i ] == candidate ) {
+      return false;
+    }
+  }
+  for ( let i = 0; i < 9; ++i ) {
+    if ( matrix[ i ][ column ] == candidate ) {
+      return false;
+    }
+  }
+
+  const rowBegin = Math.floor( row / 3 ) * 3;
+  const rowEnd = rowBegin + 3;
+  const columnBegin = Math.floor( column / 3 ) * 3;
+  const columnEnd = columnBegin + 3;
+
+  for ( let i = rowBegin; i < rowEnd; ++i ) {
+    for ( let j = columnBegin; j < columnEnd; ++j ) {
+      if ( matrix[ i ][ j ] == candidate ) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
